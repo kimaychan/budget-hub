e40909f8274b02395bd62c23b9c39dd4fa5e684a
Mikee Rose I. Bersabal <mribersabal@vauldex.com>
2019-09-25 12:20:47 +0800

    Implement add wallet UI

--- src/components/wallet-list.vue
+++ src/components/wallet-list.vue
@@ -2,8 +2,17 @@
   <div>
     <ul class="wallet-list">
       <li :key="i" v-for="(w, i) in wallets">
-        <div class="wallet-wrapper" @click="$emit('walletclicked', w)">
-          <wallet :currency="w.currency" :name="w.name" :balance="w.balance"></wallet>
+        <div @click="$emit('walletclicked', w)" >
+          <wallet 
+            :currency="w.currency" 
+            :name="w.name" 
+            :balance="w.balance"
+          ></wallet>
+        </div>
+      </li>
+      <li>
+        <div @click="$emit('walletclicked', {})">
+          <add-wallet></add-wallet>
         </div>
       </li>
     </ul>
@@ -11,13 +20,15 @@
 </template>
 <script>
 import Wallet from "./wallet.vue"
+import AddWallet from "./add-wallet.vue"
+
 export default {
   name: "WalletList",
   props: [ "wallets" ],
   data: function () { 
     return {}
   },
-  components: { Wallet }
+  components: { Wallet, AddWallet }
 }
 </script>
 <style lang="scss" scoped>
--- src/components/wallet.vue
+++ src/components/wallet.vue
@@ -13,27 +13,5 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-  .wallet {
-    background-color: rgb(15, 20, 36);
-    padding: 2rem 3rem;
-    text-align: center;
-    border-radius: .1rem;
-    box-sizing: border-box;
-    .balance {
-      font-size: 120%;
-      color: rgb(220, 220, 220);
-    }
-    .name {
-      font-size: 60%;
-      display: block;
-      color: rgb(95, 93, 112);
-    }
-    &:hover {
-      background-color: rgba(49, 51, 71, 0.363);
-      cursor: pointer;
-    }
-  }
-</style>
 
 
