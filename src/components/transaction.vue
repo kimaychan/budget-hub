80f5a5036e668c0cc0c43bb0dbeb616ae2f59e63
Mikee Rose I. Bersabal <mribersabal@vauldex.com>
2019-09-25 12:51:56 +0800

    Edit transaction styles

--- src/components/transaction.vue
+++ src/components/transaction.vue
@@ -17,43 +17,5 @@ export default {
   props: ["date", "destination", "amountCurrency", "amountValue", "purpose", "type"]
 }
 </script>
-<style lang="scss" scoped>
-  .transaction {
-    padding: 0.5rem;
-    box-sizing: border-box;
-    background-color: rgb(15, 20, 36);
-    border-radius: .1rem;
-    color: rgb(125, 125, 139);
-    font-size: .8rem;
-    &:hover {
-      background-color: rgba(49, 51, 71, 0.363);
-      cursor: pointer;
-    }
-    table {
-      width: 100%;
-      tr {
-        td:first-child, td:last-child {
-          width: 10%;
-        }
-        td:last-child {
-          text-align: right;
-        }
-      }
-    }
-    .income { 
-      color: #30ac57; 
-      &::before {
-        content: "+"
-      }
-    }
-    .expense { 
-      color: #C10505; 
-      &::before {
-        content: "-"
-      }
-    }
-    .transfer { color:#0A68D2; }
-  }
-</style>
 
 
